2ff9534469d0746a96b49843687df4e4da30ef02
Nathan Silva <nathansilva.dev@gmail.com>
2025-09-26 01:56:36 -0300

    cors

--- backend/main.py
+++ backend/main.py
@@ -3,11 +3,21 @@ from sqlalchemy.orm import Session
 import models, schemas, crud
 from database import engine, Base, get_db
 from typing import List
+from fastapi import HTTPException
+from fastapi.middleware.cors import CORSMiddleware
 import uuid
 
 Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 # Tasks
 @app.post("/tasks/", response_model=schemas.Task)
@@ -22,7 +32,6 @@ def read_tasks(skip: int = 0, limit: int = 10, db: Session = Depends(get_db)):
 def read_task(task_id: uuid.UUID, db: Session = Depends(get_db)):
     task = crud.get_task(db, str(task_id))
     if not task:
-        from fastapi import HTTPException
         raise HTTPException(status_code=404, detail="Task not found")
     return task
 
@@ -32,5 +41,4 @@ def update_task(task_id: uuid.UUID, task: schemas.TaskCreate, db: Session = Depe
 
 @app.delete("/tasks/{task_id}")
 def delete_task(task_id: uuid.UUID, db: Session = Depends(get_db)):
-    crud.delete_task(db, str(task_id))
-    return {"ok": True}
+    return crud.delete_task(db, str(task_id))
